9f0330e9b4165a29f4cc3b0bce83e897d92d9e90
Djordje050182 <dgvozdenovic@gmail.com>
2026-01-29 14:10:26 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+base: '/financetrackerv2/'
+```
+5. Commit changes
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/finance-tracker/'
-})
+### **Step 2: Wait 2 Minutes**
+
+GitHub Actions will rebuild automatically with the correct path.
+
+### **Step 3: Try URL Again**
+
+Your URL should be:
+```
+https://djordje050182.github.io/financetrackerv2/
