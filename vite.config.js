d2a7ba71116a55a09d20a28e51d7fe51798ca313
Djordje050182 <dgvozdenovic@gmail.com>
2026-01-29 14:17:29 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
-  base: '/financetrackerv2/'
+plugins: [react()],
+base: '/financetrackerv2/'
 })
-```
-
-### **Step 4: Make Sure It Looks Exactly Like This:**
-```
-Line 1: import { defineConfig } from 'vite'
-Line 2: import react from '@vitejs/plugin-react'
-Line 3: (blank)
-Line 4: export default defineConfig({
-Line 5:   plugins: [react()],
-Line 6:   base: '/financetrackerv2/'
-Line 7: })
